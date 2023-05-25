e41e2fb334595b2459a0efe1117b27c9a10fe43b
niranjanchechani1 <133879186+niranjanchechani1@users.noreply.github.com>
2023-05-26 01:13:11 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "chanzuckerberg/snowflake"
+      version = "0.25.17"
+    }
+  }
+
+  backend "remote" {
+    organization = "SNOWFLAKECICD"
+
+    workspaces {
+      name = "gh-actions-demo2"
+    }
+  }
+}
+
+provider "snowflake" {
+}
+
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
+  comment = "Database for Snowflake Terraform demo"
+}
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
